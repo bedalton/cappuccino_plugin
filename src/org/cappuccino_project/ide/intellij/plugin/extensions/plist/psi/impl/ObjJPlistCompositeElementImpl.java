c2f7bb38d57f57e16dcefdf56c74a6961646a4ee
daniel <dsbadal@gmail.com>
2018-04-18 15:20:58 -0700

    still trying to do a basic commit

--- src/org/cappuccino_project/ide/intellij/plugin/extensions/plist/psi/impl/ObjJPlistCompositeElementImpl.java
+++
@@ -1,4 +0,0 @@
-package org.cappuccino_project.ide.intellij.plugin.extensions.plist.psi.impl;
-
-public class ObjJPlistCompositeElementImpl {
-}
--- src/org/cappuccino_project/ide/intellij/plugin/psi/interfaces/ObjJStubBasedElement.java
+++
@@ -1,8 +0,0 @@
-package org.cappuccino_project.ide.intellij.plugin.psi.interfaces;
-
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.StubBasedPsiElement;
-import com.intellij.psi.stubs.StubElement;
-
-public interface ObjJStubBasedElement<StubT extends StubElement<? extends PsiElement>> extends  StubBasedPsiElement<StubT> {
-}
