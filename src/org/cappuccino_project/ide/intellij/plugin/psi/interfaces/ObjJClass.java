34a18b6d4a1ed4d92c1c6369d95c430b1ca4f173
daniel <dsbadal@gmail.com>
2018-04-18 15:13:00 -0700

    .gitignore fix

--- src/org/cappuccino_project/ide/intellij/plugin/psi/interfaces/ObjJClass.java
+++
@@ -1,5 +0,0 @@
-package org.cappuccino_project.ide.intellij.plugin.psi.interfaces;
-
-public interface ObjJClass {
-
-}
--- src/org/cappuccino_project/ide/intellij/plugin/psi/interfaces/ObjJQualifiedNamePart.java
+++
@@ -1,8 +0,0 @@
-package org.cappuccino_project.ide.intellij.plugin.psi.interfaces;
-
-import javax.annotation.Nullable;
-
-public interface ObjJQualifiedNamePart extends ObjJCompositeElement {
-    @Nullable
-    String getQualifiedNameText();
-}
