722d9d2e883c57a952bd1770c563ed334d1767f1
daniel <dsbadal@gmail.com>
2018-05-20 08:15:04 -0700

    update selector insert handler

--- src/cappuccino/ide/intellij/plugin/contributor/handlers/ObjJSelectorInsertHandler.kt
+++ src/cappuccino/ide/intellij/plugin/contributor/handlers/ObjJSelectorInsertHandler.kt
@@ -10,11 +10,18 @@ class ObjJSelectorInsertHandler private constructor() : InsertHandler<LookupElem
 
     override fun handleInsert(insertionContext: InsertionContext, lookupElement: LookupElement) {
         insertColon(insertionContext, lookupElement)
+        insertSpaceBefore(insertionContext) // must be after colon is inserted, otherwise the colon would be inserted in the wrong spot
     }
 
     private fun insertColon(insertionContext: InsertionContext, lookupElement: LookupElement) {
         if (!EditorUtil.isTextAtOffset(insertionContext, ObjJMethodPsiUtils.SELECTOR_SYMBOL)) {
             EditorUtil.insertText(insertionContext, ObjJMethodPsiUtils.SELECTOR_SYMBOL, true)
+        }
+    }
+
+    private fun insertSpaceBefore(insertionContext: InsertionContext) {
+        if (!EditorUtil.isTextAtOffset(insertionContext.document, insertionContext.startOffset-1, " ")) {
+            EditorUtil.insertText(insertionContext.editor, " ", insertionContext.startOffset, false)
         }
     }
 
