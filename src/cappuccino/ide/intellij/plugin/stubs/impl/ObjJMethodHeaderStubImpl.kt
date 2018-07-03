400ea6e1678024ab238f971a499dc28e17de6662
daniel <dsbadal@gmail.com>
2018-07-03 12:22:57 -0700

    Update: updated method header stub to inline variable assignments with defaults

--- src/cappuccino/ide/intellij/plugin/stubs/impl/ObjJMethodHeaderStubImpl.kt
+++ src/cappuccino/ide/intellij/plugin/stubs/impl/ObjJMethodHeaderStubImpl.kt
@@ -8,18 +8,12 @@ import cappuccino.ide.intellij.plugin.psi.types.ObjJClassType
 import cappuccino.ide.intellij.plugin.psi.utils.ObjJMethodPsiUtils
 
 class ObjJMethodHeaderStubImpl(parent: StubElement<*>?, className: String?, override val isStatic: Boolean, override val selectorStrings: List<String>, override val paramTypes: List<String>, returnType: String?, override val isRequired: Boolean, private val shouldResolve: Boolean) : ObjJStubBaseImpl<ObjJMethodHeaderImpl>(parent, ObjJStubTypes.METHOD_HEADER), ObjJMethodHeaderStub {
-    override val returnTypeAsString: String
-    override val containingClassName: String
-    override val selectorString: String
+    override val returnTypeAsString: String = returnType ?: ObjJClassType.UNDETERMINED
+    override val containingClassName: String = className ?: ObjJClassType.UNDEF_CLASS_NAME
+    override val selectorString: String = ObjJMethodPsiUtils.getSelectorStringFromSelectorStrings(selectorStrings)
 
     override val returnType: ObjJClassType
         get() = ObjJClassType.getClassType(returnTypeAsString)
-
-    init {
-        this.selectorString = ObjJMethodPsiUtils.getSelectorStringFromSelectorStrings(selectorStrings)
-        this.returnTypeAsString = returnType ?: ObjJClassType.UNDETERMINED
-        this.containingClassName = className ?: ObjJClassType.UNDEF_CLASS_NAME
-    }
 
     override fun shouldResolve(): Boolean {
         return shouldResolve
