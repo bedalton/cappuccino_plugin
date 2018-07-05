2067a2d833aa0198840a8e7be8d58b50a16c5828
bedalton <dsbadal@gmail.com>
2018-07-05 11:07:50 -0700

    Update: made classname a stub element

---
+++ src/cappuccino/ide/intellij/plugin/stubs/interfaces/ObjJClassNameStub.kt
@@ -0,0 +1,8 @@
+package cappuccino.ide.intellij.plugin.stubs.interfaces
+
+import cappuccino.ide.intellij.plugin.psi.impl.ObjJClassNameImpl
+import com.intellij.psi.stubs.StubElement
+
+interface ObjJClassNameStub : StubElement<ObjJClassNameImpl> {
+    fun getClassName() : String
+}
---
+++ src/cappuccino/ide/intellij/plugin/stubs/types/ObjJClassNameStubType.kt
@@ -0,0 +1,39 @@
+package cappuccino.ide.intellij.plugin.stubs.types
+
+import cappuccino.ide.intellij.plugin.psi.impl.ObjJClassNameImpl
+import cappuccino.ide.intellij.plugin.stubs.impl.ObjJClassNameStubImpl
+import cappuccino.ide.intellij.plugin.stubs.interfaces.ObjJClassNameStub
+import com.intellij.psi.stubs.StubElement
+import com.intellij.psi.stubs.StubInputStream
+import com.intellij.psi.stubs.StubOutputStream
+import com.intellij.util.io.StringRef
+
+import java.io.IOException
+
+class ObjJClassNameStubType(
+        debugName: String) : ObjJStubElementType<ObjJClassNameStub, ObjJClassNameImpl>(debugName, ObjJClassNameImpl::class.java) {
+
+    override fun createPsi(
+            stub: ObjJClassNameStub): ObjJClassNameImpl {
+        return ObjJClassNameImpl(stub, ObjJStubTypes.CLASS_NAME)
+    }
+
+    override fun createStub(
+            className: ObjJClassNameImpl, parent: StubElement<*>): ObjJClassNameStub {
+        return ObjJClassNameStubImpl(parent, className.text)
+    }
+
+    @Throws(IOException::class)
+    override fun serialize(
+            stub: ObjJClassNameStub,
+            stream: StubOutputStream) {
+        stream.writeName(stub.getClassName())
+    }
+
+    @Throws(IOException::class)
+    override fun deserialize(
+            stream: StubInputStream, parent: StubElement<*>): ObjJClassNameStub {
+        val className:StringRef = stream.readName() ?: StringRef.fromString("");
+        return ObjJClassNameStubImpl(parent,className)
+    }
+}
