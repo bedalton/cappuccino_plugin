b02ba35073cef373ea20d396e910980fe224e3d3
bedalton <dsbadal@gmail.com>
2018-07-05 12:14:57 -0700

    Fix: Class name reference resolution

--- src/cappuccino/ide/intellij/plugin/references/ObjJClassNameReference.kt
+++ src/cappuccino/ide/intellij/plugin/references/ObjJClassNameReference.kt
@@ -2,23 +2,53 @@ package cappuccino.ide.intellij.plugin.references
 
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElementResolveResult
-import com.intellij.psi.PsiPolyVariantReferenceBase
-import com.intellij.psi.ResolveResult
 import cappuccino.ide.intellij.plugin.indices.ObjJClassDeclarationsIndex
 import cappuccino.ide.intellij.plugin.psi.ObjJClassName
 import cappuccino.ide.intellij.plugin.psi.interfaces.ObjJClassDeclarationElement
+import cappuccino.ide.intellij.plugin.psi.types.ObjJTypes
+import cappuccino.ide.intellij.plugin.psi.utils.getPreviousNonEmptyNode
+import cappuccino.ide.intellij.plugin.psi.utils.getPreviousNonEmptySibling
+import com.intellij.openapi.progress.ProgressIndicatorProvider
+import com.intellij.psi.*
+import javafx.scene.control.ProgressIndicator
 
 import java.util.ArrayList
 
 class ObjJClassNameReference(element: ObjJClassName) : PsiPolyVariantReferenceBase<ObjJClassName>(element, TextRange.create(0, element.textLength)) {
     private val className: String? = element.text
+    private val isClassDeclarationName:Boolean = myElement.parent as? ObjJClassDeclarationElement<*> != null && myElement.getPreviousNonEmptySibling(true)?.text ?: "" != ":"
+
+    override fun handleElementRename(newElementName: String?): PsiElement {
+        return if (myElement is ObjJClassName && newElementName != null)  myElement.setName(newElementName) else myElement
+    }
+
+    override fun isReferenceTo(element: PsiElement?): Boolean {
+        if (className == null) {
+            return false
+        }
+        if (element !is ObjJClassName || !element.text.equals(className)) {
+            return false
+        }
+
+        if (super.isReferenceTo(element)) {
+            return true
+        }
+
+        if (isClassDeclarationName) {
+            return element.parent !is ObjJClassDeclarationElement<*>
+        }
+        return element.parent is ObjJClassDeclarationElement<*>
+    }
 
     override fun multiResolve(b: Boolean): Array<ResolveResult> {
         if (className == null) {
             return arrayOf()
         }
+        ProgressIndicatorProvider.checkCanceled()
         if (DumbService.isDumb(myElement.project)) {
+            return ResolveResult.EMPTY_ARRAY
+        }
+        if (isClassDeclarationName) {
             return ResolveResult.EMPTY_ARRAY
         }
         val classNames = ArrayList<ObjJClassName>()
