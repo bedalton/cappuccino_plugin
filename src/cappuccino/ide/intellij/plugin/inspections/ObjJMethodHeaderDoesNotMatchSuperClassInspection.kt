55faa446db2e06f47ddc4f619daeef2c4c5d765a
daniel <dsbadal@gmail.com>
2018-07-27 10:20:52 -0700

    added quickfix for method override mismatch parameters

--- src/cappuccino/ide/intellij/plugin/inspections/ObjJMethodHeaderDoesNotMatchSuperClassInspection.kt
+++ src/cappuccino/ide/intellij/plugin/inspections/ObjJMethodHeaderDoesNotMatchSuperClassInspection.kt
@@ -1,10 +1,12 @@
 package cappuccino.ide.intellij.plugin.inspections
 
+import cappuccino.ide.intellij.plugin.fixes.ObjJChangeVarTypeToMatchQuickFix
 import cappuccino.ide.intellij.plugin.indices.ObjJUnifiedMethodIndex
 import cappuccino.ide.intellij.plugin.psi.ObjJMethodHeader
 import cappuccino.ide.intellij.plugin.psi.ObjJVisitor
 import cappuccino.ide.intellij.plugin.utils.ObjJInheritanceUtil
 import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import java.util.*
@@ -15,7 +17,7 @@ class ObjJMethodHeaderDoesNotMatchSuperClassInspection : LocalInspectionTool() {
         return "MethodHeaderDoesNotMatchSuperClass"
     }
 
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    override fun    buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : ObjJVisitor() {
             override fun visitMethodHeader(header: ObjJMethodHeader) {
                 validateMethodHeader(header, holder)
@@ -42,20 +44,38 @@ class ObjJMethodHeaderDoesNotMatchSuperClassInspection : LocalInspectionTool() {
             }
         }
         private fun matches(thisHeader:ObjJMethodHeader, thatHeader:ObjJMethodHeader, problemsHolder: ProblemsHolder) : Boolean {
+            var matches = true
             if (thisHeader.returnType != thatHeader.returnType) {
-                if (thisHeader.methodHeaderReturnTypeElement != null) {
-                    problemsHolder.registerProblem(thisHeader.methodHeaderReturnTypeElement!!, "Overridden method should have return type <" + thatHeader.returnType + ">")
+                val methodHeaderReturnTypeElement = thisHeader.methodHeaderReturnTypeElement
+                if (methodHeaderReturnTypeElement != null) {
+                    val thisHeaderReturnType = methodHeaderReturnTypeElement.formalVariableType
+                    if (!(thisHeaderReturnType.text?.toLowerCase() != "void" && thatHeader.returnType.toLowerCase() == "id")) {
+                        problemsHolder.registerProblem(thisHeaderReturnType, "Overridden parent is less specific", ProblemHighlightType.INFORMATION, ObjJChangeVarTypeToMatchQuickFix(thisHeaderReturnType, thatHeader.returnType))
+                    } else {
+                        problemsHolder.registerProblem(thisHeader.methodHeaderReturnTypeElement!!, "Overridden method should have return type <" + thatHeader.returnType + ">", ObjJChangeVarTypeToMatchQuickFix(thisHeader, thatHeader.returnType))
+                    }
                 }
-                return false
+                matches = false
             }
             val thoseSelectors = thatHeader.methodDeclarationSelectorList
-            var matches = true;
+
             for ((i, selector) in thisHeader.methodDeclarationSelectorList.withIndex()) {
                 val otherParam = thoseSelectors[i].varType ?: continue
                 val thisVarType = selector.varType
                 if (!Objects.equals(thisVarType?.text,otherParam.text)) {
-                    problemsHolder.registerProblem((thisVarType ?: selector), "Parameter should have type <" + otherParam.text + ">")
-                    matches = false
+                    if (thisVarType != null && !(thisVarType.text?.toLowerCase() != "void" && otherParam.text.toLowerCase() == "id")) {
+                        problemsHolder.registerProblem(thisVarType, "Overrides parent type has less specific variable type of id")
+                    } else {
+                        val errorMessage = "Parameter should have type <" + otherParam.text + ">";
+                        if (thisVarType != null) {
+                            problemsHolder.registerProblem((thisVarType), errorMessage, ObjJChangeVarTypeToMatchQuickFix(thisVarType, otherParam.text))
+                        } else if (selector.openParen != null && selector.closeParen != null) {
+                            problemsHolder.registerProblem(selector, errorMessage, ObjJChangeVarTypeToMatchQuickFix(selector, otherParam.text))
+                        } else {
+                            problemsHolder.registerProblem(selector, errorMessage)
+                        }
+                        matches = false
+                    }
                 }
             }
             return matches
