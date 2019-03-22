b26725bde5054c6db268ebf121089ffb1139e36f
bedalton <dsbadal@gmail.com>
2019-03-22 07:12:37 -0700

    removed unused stub interfaces

--- src/cappuccino/ide/intellij/plugin/stubs/interfaces/ObjJIncludeFrameworkStub.kt
+++
@@ -1,5 +0,0 @@
-package cappuccino.ide.intellij.plugin.stubs.interfaces
-
-import cappuccino.ide.intellij.plugin.psi.impl.ObjJIncludeFrameworkImpl
-
-interface ObjJIncludeFrameworkStub : ObjJImportStub<ObjJIncludeFrameworkImpl>
--- src/cappuccino/ide/intellij/plugin/stubs/interfaces/ObjJPlaceHolderStub.kt
+++
@@ -1,7 +0,0 @@
-package cappuccino.ide.intellij.plugin.stubs.interfaces
-
-
-import com.intellij.psi.stubs.StubElement
-import cappuccino.ide.intellij.plugin.psi.interfaces.ObjJCompositeElement
-
-internal interface ObjJPlaceHolderStub<T : ObjJCompositeElement> : StubElement<T>
