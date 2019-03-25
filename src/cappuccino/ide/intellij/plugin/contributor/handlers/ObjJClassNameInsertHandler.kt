cc621bb354497167ac0da87b4a32b5b7a4857c36
daniel <dsbadal@gmail.com>
2019-03-25 07:04:56 -0700

    cleaned up logic and added comments

--- src/cappuccino/ide/intellij/plugin/contributor/handlers/ObjJClassNameInsertHandler.kt
+++ src/cappuccino/ide/intellij/plugin/contributor/handlers/ObjJClassNameInsertHandler.kt
@@ -13,38 +13,55 @@ import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.psi.PsiElement
 
+/**
+ * Handles the insertion of class names
+ * Mostly used for method selector declarations
+ */
 object ObjJClassNameInsertHandler : InsertHandler<LookupElement> {
 
+    /**
+     * Handles the aftermath of inserting a class name
+     */
     override fun handleInsert(insertionContext: InsertionContext, lookupElement: LookupElement) {
         val thisElement: PsiElement = lookupElement.psiElement ?: return
         when {
-            thisElement.hasParentOfType(ObjJMethodDeclarationSelector::class.java) -> {
-                val nextElement: PsiElement = thisElement.getNextNonEmptySibling(true) ?: return
-                when {
-                    nextElement.text != ")" -> {
-                        EditorUtil.insertText(insertionContext, ")", true)
-                        if (EditorUtil.isTextAtOffset(insertionContext.document, insertionContext.selectionEndOffset + 1, " ")) {
-                            EditorUtil.insertText(insertionContext, " ", true)
-                        }
-                    }
-                    else -> {
-                        if (nextElement.text == ")") {
-                            insertionContext.editor.caretModel.moveToOffset(nextElement.textRange.endOffset)
-                        }
-                    }
-                }
-                return
-            }
+            thisElement.hasParentOfType(ObjJMethodDeclarationSelector::class.java) ->
+                insertInMethodDeclarationSelector(thisElement, insertionContext)
+            thisElement.hasParentOfType(ObjJInheritedProtocolList::class.java) ->
+                insertInInheritedProtocolList(thisElement, insertionContext)
+        }
+    }
 
-            thisElement.hasParentOfType(ObjJInheritedProtocolList::class.java) -> {
-                val nextElement = thisElement.getNextNonEmptySibling(true)
-                when {
-                    nextElement == null -> EditorUtil.insertText(insertionContext, ">", false)
-                    nextElement.text == "," -> EditorUtil.offsetCaret(insertionContext, 1)
-                    nextElement is ObjJClassName -> EditorUtil.insertText(insertionContext, ",", true)
-                    nextElement.tokenType() != ObjJTypes.ObjJ_GREATER_THAN -> EditorUtil.insertText(insertionContext, ">", false)
+    /**
+     * Handles insertions while inside method declaration selector
+     */
+    private fun insertInMethodDeclarationSelector(thisElement:PsiElement, insertionContext: InsertionContext) {
+        val nextElement: PsiElement = thisElement.getNextNonEmptySibling(true) ?: return
+        when {
+            nextElement.text != ")" -> {
+                EditorUtil.insertText(insertionContext, ")", true)
+                if (EditorUtil.isTextAtOffset(insertionContext.document, insertionContext.selectionEndOffset + 1, " ")) {
+                    EditorUtil.insertText(insertionContext, " ", true)
                 }
             }
+            else -> {
+                if (nextElement.text == ")") {
+                    insertionContext.editor.caretModel.moveToOffset(nextElement.textRange.endOffset)
+                }
+            }
+        }
+    }
+
+    /**
+     * Handles insertion while in inherited protocol list
+     */
+    private fun insertInInheritedProtocolList(thisElement:PsiElement, insertionContext: InsertionContext) {
+        val nextElement = thisElement.getNextNonEmptySibling(true)
+        when {
+            nextElement == null -> EditorUtil.insertText(insertionContext, ">", false)
+            nextElement.text == "," -> EditorUtil.offsetCaret(insertionContext, 1)
+            nextElement is ObjJClassName -> EditorUtil.insertText(insertionContext, ",", true)
+            nextElement.tokenType() != ObjJTypes.ObjJ_GREATER_THAN -> EditorUtil.insertText(insertionContext, ">", false)
         }
     }
 }
