932218bfbe242bc586f6c46b5beca11bfd3ce0f4
bedalton <dsbadal@gmail.com>
2019-03-30 12:12:43 -0700

    improved return type inference

--- src/cappuccino/ide/intellij/plugin/references/ObjJReturnTypeResolveUtil.kt
+++ src/cappuccino/ide/intellij/plugin/references/ObjJReturnTypeResolveUtil.kt
@@ -1,13 +1,21 @@
 package cappuccino.ide.intellij.plugin.references
 
 import cappuccino.ide.intellij.plugin.contributor.ObjJVariableTypeResolver
+import cappuccino.ide.intellij.plugin.indices.ObjJUnifiedMethodIndex
 import cappuccino.ide.intellij.plugin.psi.*
 import cappuccino.ide.intellij.plugin.psi.interfaces.ObjJHasMethodSelector
 import cappuccino.ide.intellij.plugin.psi.types.ObjJClassType
+import cappuccino.ide.intellij.plugin.psi.utils.ObjJHasContainingClassPsiUtil
+import cappuccino.ide.intellij.plugin.psi.utils.ObjJVariableNameAggregatorUtil
 import cappuccino.ide.intellij.plugin.psi.utils.getParentOfType
 import cappuccino.ide.intellij.plugin.utils.ObjJInheritanceUtil
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
 
 
+/**
+ * Attempts to get possible variable types from a selector
+ */
 fun getClassConstraints(element: ObjJSelector): List<String> {
     return getClassConstraints(element.getParentOfType( ObjJHasMethodSelector::class.java))
 }
@@ -20,28 +28,45 @@ private fun getClassConstraints(element: ObjJHasMethodSelector?): List<String> {
     val methodCall = element as ObjJMethodCall?
     val callTarget: ObjJCallTarget? = methodCall?.callTarget
     if (callTarget != null) {
-        val possibleClasses = getPossibleClassTypesForCallTarget(callTarget)
-        if (possibleClasses.isNotEmpty()) {
-            return possibleClasses.toList()
+        val out = getPossibleClassTypesForCallTarget(callTarget)
+        if (out.isNotEmpty()) {
+            return out.toList()
         }
     }
     return methodCall?.callTarget?.possibleCallTargetTypes ?: mutableListOf()
 }
 
+/**
+ * Attempts to get possible class types for a call target
+ */
 fun getPossibleClassTypesForCallTarget(callTarget: ObjJCallTarget) : Set<String> {
     val qualifiedReference = callTarget.qualifiedReference ?: return setOf()
     val methodCall = qualifiedReference.methodCall
+    val project = callTarget.project
     if (methodCall != null) {
-        if (methodCall.selector?.text == "alloc") {
-            return ObjJInheritanceUtil.getAllInheritedClasses(methodCall.callTargetText, methodCall.project, true)
-        }
+        return getPossibleCallTargetTypeFromMethodCall(methodCall)
+                .flatMap {
+                    val out = mutableListOf(it)
+                    out.addAll(ObjJInheritanceUtil.getAllInheritedClasses(it, project, true))
+                    out
+                }.toSet()
     }
-    val variables = qualifiedReference.variableNameList
 
-    if (variables.size != 1) {
-        return setOf()
-    }
-    val variableName = variables[0]
+    // Get if variable name
+    val variableName = qualifiedReference.qualifiedNameParts.getOrNull(0) as? ObjJVariableName
+            ?: return setOf()
+    return getPossibleTypesIfVariableName(variableName)
+            .flatMap {
+                val out = mutableListOf(it)
+                out.addAll(ObjJInheritanceUtil.getAllInheritedClasses(it, project, true))
+                out
+            }.toSet()
+}
+
+/**
+ * Attempt to get call call target type if variable name
+ */
+private fun getPossibleTypesIfVariableName(variableName:ObjJVariableName) : Set<String> {
     val variableNameText = variableName.text
     val className = when (variableNameText) {
         "self" -> variableName.containingClassName
@@ -50,7 +75,7 @@ fun getPossibleClassTypesForCallTarget(callTarget: ObjJCallTarget) : Set<String>
             ObjJIgnoreEvaluatorUtil.getVariableTypesInParent(variableName) ?: getTypeFromInstanceVariables(variableName)
         }
     } ?: return ObjJVariableTypeResolver.resolveVariableType(variableName)
-    return ObjJInheritanceUtil.getAllInheritedClasses(className, callTarget.project, true)
+    return ObjJInheritanceUtil.getAllInheritedClasses(className, variableName.project, true)
 }
 
 /**
@@ -65,4 +90,115 @@ private fun getTypeFromInstanceVariables(variableName: ObjJVariableName) : Strin
         return type.varTypeId?.className?.text ?: ObjJClassType.UNDETERMINED
     }
     return type.text
+}
+
+
+/**
+ * Attempts to get possible call target type if method call
+ */
+private fun getPossibleCallTargetTypeFromMethodCall(methodCall:ObjJMethodCall, follow:Boolean = true) : Set<String> {
+    if (methodCall.selector?.text == "alloc") {
+        return ObjJInheritanceUtil.getAllInheritedClasses(methodCall.callTargetText, methodCall.project, true)
+    }
+
+    if (methodCall.selectorList.size == 1) {
+        val out = getSimpleTargetTypesIfAccessor(methodCall, follow)
+        if (out.isNotEmpty())
+            return out
+    }
+    return getPossibleCallTargetTypesFromMultiSelectorCall(methodCall)
+}
+
+/**
+ * Attempts to get target type from simple self or super calls to instance variables
+ */
+private fun getSimpleTargetTypesIfAccessor(methodCall: ObjJMethodCall, follow:Boolean = true) : Set<String> {
+
+    val selectorVariableName = methodCall.selectorList[0].getSelectorString(false)
+    // Attempts to get simple containing class target
+    val containingClass = when (methodCall.callTarget.text) {
+        "self" -> ObjJHasContainingClassPsiUtil.getContainingClassName(methodCall)
+        "super" -> ObjJHasContainingClassPsiUtil.getContainingSuperClassName(methodCall)
+        else -> null
+    }
+
+    val project:Project = methodCall.project
+    if (containingClass == null) {
+        // If should not go recursive
+        if (!follow) {
+            return setOf()
+        }
+        val callTargetAsMethodCall = methodCall.callTarget.expr?.leftExpr?.methodCall
+        if (callTargetAsMethodCall == null || methodCall.callTarget.expr?.rightExprList?.isNotEmpty() == true) {
+            return setOf()
+        }
+        val containingClasses = getPossibleCallTargetTypeFromMethodCall(callTargetAsMethodCall, false)
+        val out = mutableListOf<String>()
+        containingClasses.forEach{
+            out.addAll(getInstanceVariableTypesForClass(it, selectorVariableName, project))
+        }
+        return out.toSet()
+    }
+
+    return getInstanceVariableTypesForClass(containingClass, selectorVariableName, project)
+}
+
+private fun getInstanceVariableTypesForClass(containingClass:String, selectorVariableName:String, project:Project) : Set<String> {
+    // Tries to get instance variables for tyoe
+    val instanceVariables = ObjJVariableNameAggregatorUtil
+            .getAllContainingClassInstanceVariables(containingClass, project)
+            .filter {
+                selectorVariableName in getInstanceVariableAndAccessorsAsStringList(it)
+            }
+
+    // If no instance variables found, return
+    if (instanceVariables.isEmpty())
+        return setOf()
+
+    // Gets all variables types in instance variables
+    val out: List<String> = getVariableTypesFromInstanceVariableNames(instanceVariables)
+    return out.toSet()
+}
+
+
+/**
+ * Gets possible call target types from compound selectors
+ * Very simple implementation
+ * Most object return types return id, making it near useless
+ */
+private fun getPossibleCallTargetTypesFromMultiSelectorCall(methodCall: ObjJMethodCall) : Set<String> {
+    if (DumbService.isDumb(methodCall.project))
+        return setOf()
+    val selector = methodCall.selectorString
+    return ObjJUnifiedMethodIndex.instance[selector, methodCall.project]
+            .map {
+                it.returnType
+            }
+            .filterNot {
+                it.startsWith("@") || it == "IBAction" || it == "IBAction" || it == "void"
+            }.toSet()
+}
+
+/**
+ * Returns a list containing am instnace variables accessors and name as strings
+ */
+private fun getInstanceVariableAndAccessorsAsStringList(it:ObjJVariableName) : List<String> {
+    val out:MutableList<String> = mutableListOf(it.text)
+    val parent = it.getParentOfType(ObjJInstanceVariableDeclaration::class.java) ?: return out
+    val accessors = parent.accessorPropertyList.filterNot { it.accessorPropertyType.text == "setter" }.mapNotNull { it.accessor?.text }
+    out.addAll(accessors)
+    return out
+}
+
+/**
+ * Attempts to get variable types from a list of instance variables
+ */
+private fun getVariableTypesFromInstanceVariableNames(instanceVariables:List<ObjJVariableName>) : List<String> {
+    return instanceVariables.mapNotNull {
+        val formalVariableType = it.getParentOfType(ObjJInstanceVariableDeclaration::class.java)?.formalVariableType
+        if (formalVariableType?.varTypeId?.className != null)
+            formalVariableType.varTypeId?.className?.text
+        else
+            formalVariableType?.text
+    }
 }
