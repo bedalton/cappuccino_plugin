267615659f2d0f6353aa6fc923bf68d74bdc957b
bedalton <dsbadal@gmail.com>
2019-05-31 18:20:34 -0700

    added variable declaration stubs

---
+++ src/cappuccino/ide/intellij/plugin/stubs/interfaces/ObjJVariableDeclarationStub.kt
@@ -0,0 +1,8 @@
+package cappuccino.ide.intellij.plugin.stubs.interfaces
+
+import cappuccino.ide.intellij.plugin.psi.impl.ObjJVariableDeclarationImpl
+import com.intellij.psi.stubs.StubElement
+
+interface ObjJVariableDeclarationStub : StubElement<ObjJVariableDeclarationImpl> {
+    val qualifiedNameParts:List<QualifiedReferenceStubComponents>
+}
