3a5db948648f7f3baef938a86ece88f8425a655d
bedalton <dsbadal@gmail.com>
2019-06-15 09:18:34 -0700

    Update ObjJStubVersions.kt

--- src/cappuccino/ide/intellij/plugin/stubs/ObjJStubVersions.kt
+++ src/cappuccino/ide/intellij/plugin/stubs/ObjJStubVersions.kt
@@ -1,7 +1,7 @@
 package cappuccino.ide.intellij.plugin.stubs
 
 object ObjJStubVersions {
-    private const val MINOR_VERSION = 1
+    private const val MINOR_VERSION = 2
     private const val MAJOR_VERSION = 7
     const val SOURCE_STUB_VERSION = MAJOR_VERSION + MINOR_VERSION
 }
