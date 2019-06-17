4516f2f79b5d89d6222780c1b98ab3c99111e735
daniel <dsbadal@gmail.com>
2019-06-17 16:23:07 -0700

    stop autosuggest in property names

--- src/cappuccino/ide/intellij/plugin/contributor/ObjJBlanketCompletionProvider.kt
+++ src/cappuccino/ide/intellij/plugin/contributor/ObjJBlanketCompletionProvider.kt
@@ -132,6 +132,9 @@ object ObjJBlanketCompletionProvider : CompletionProvider<CompletionParameters>(
                 resultSet.stopHere()
                 return
             }
+            element.parent is ObjJObjectLiteral && prevSibling?.text != ":" -> {
+                resultSet.stopHere()
+            }
             prevSibling.elementType !in ObjJTokenSets.CAN_COMPLETE_AFTER ->{
                 //LOGGER.info("Cannot complete after ${prevSibling.elementType}")
                 resultSet.stopHere()
