53c8038d2418c88003bddee078f031bdc81a92b4
daniel <dsbadal@gmail.com>
2019-06-24 12:53:30 -0700

    update import

--- src/cappuccino/ide/intellij/plugin/references/ObjJFileNameAsStringLiteralReference.kt
+++ src/cappuccino/ide/intellij/plugin/references/ObjJFileNameAsStringLiteralReference.kt
@@ -8,7 +8,7 @@ import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScope
 
 class ObjJFileNameAsStringLiteralReference(element:ObjJFileNameAsImportString)
-    : PsiPolyVariantReferenceBase<ObjJFileNameAsImportString>(element, TextRange(0, element.stringLiteral.textLength - 2))
+    : PsiPolyVariantReferenceBase<ObjJFileNameAsImportString>(element, getRange(element))
 {
 
     private val fileName = element.stringLiteral.stringValue
@@ -17,7 +17,7 @@ class ObjJFileNameAsStringLiteralReference(element:ObjJFileNameAsImportString)
         return (element is PsiFile) && element.name == fileName
     }
 
-    override fun multiResolve(p0: Boolean): Array<ResolveResult> {
+    override fun multiResolve(partial: Boolean): Array<ResolveResult> {
         var directory:PsiDirectory? = element.containingFile.parent ?: return emptyArray()
         while (directory != null) {
             directory.files.forEach {
@@ -27,10 +27,17 @@ class ObjJFileNameAsStringLiteralReference(element:ObjJFileNameAsImportString)
             }
             directory = directory.parentDirectory
         }
-        val rawResult = FilenameIndex.getFilesByName(myElement.project, fileName, GlobalSearchScope.EMPTY_SCOPE).mapNotNull {
+        val rawResult = FilenameIndex.getFilesByName(myElement.project, fileName, GlobalSearchScope.everythingScope(myElement.project)).mapNotNull {
             it.firstChild
         }
         return PsiElementResolveResult.createResults(rawResult)
     }
 
+}
+
+private fun getRange(element:ObjJFileNameAsImportString) : TextRange {
+    if (element.textLength < 2)
+        return TextRange.EMPTY_RANGE
+    val offset = if (element.text.endsWith("'") || element.text.endsWith("\"")) 1 else 0
+    return TextRange(1, element.textLength - offset)
 }
