ddd8add73518974a4a94c3afafe3e9325c87f1e9
bedalton <dsbadal@gmail.com>
2019-06-24 18:16:20 -0700

    Update ObjJFrameworkUtil.kt

--- src/cappuccino/ide/intellij/plugin/utils/ObjJFrameworkUtil.kt
+++ src/cappuccino/ide/intellij/plugin/utils/ObjJFrameworkUtil.kt
@@ -3,11 +3,14 @@ package cappuccino.ide.intellij.plugin.utils
 import java.util.regex.Pattern
 
 private val bundleRegex = Pattern.compile("<key>CPBundleName</key>\\s*<string>([^<]+?)</string>")
+private val isFramweworkPlistRegex = "<key>CPBundlePackageType</key>\\s*<string>FMWK</string>".toRegex()
 
 val INFO_PLIST_FILE_NAME = "Info.plist"
 val INFO_PLIST_FILE_NAME_TO_LOWER_CASE = INFO_PLIST_FILE_NAME.toLowerCase()
 
 fun findFrameworkNameInPlistText(plistText:String) : String? {
+    if (!isFramweworkPlistRegex.containsMatchIn(plistText))
+        return null
     val matcher = bundleRegex.matcher(plistText)
     return if (matcher.find()) {
         val match = matcher.group(1)
