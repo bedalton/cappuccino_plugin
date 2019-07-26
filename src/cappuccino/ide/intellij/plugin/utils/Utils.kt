140b1d00468d3a924e5cbdce6e17b80ec6a2fba9
daniel <dsbadal@gmail.com>
2019-07-26 13:25:51 -0700

    Update Utils.kt

--- src/cappuccino/ide/intellij/plugin/utils/Utils.kt
+++ src/cappuccino/ide/intellij/plugin/utils/Utils.kt
@@ -7,3 +7,7 @@ val now:Long get() = Date().time
 fun Regex.doesNotMatch(test:String) : Boolean {
     return !matches(test)
 }
+
+fun String?.ifEmptyNull() : String? {
+    return if (isNotNullOrBlank()) this else null
+}
