773016a96d46038442b18e44ba0a67423715e6df
bedalton <dsbadal@gmail.com>
2019-07-27 07:14:01 -0700

    Update JsTypeDefStubVersion.kt

--- src/cappuccino/ide/intellij/plugin/jstypedef/stubs/JsTypeDefStubVersion.kt
+++ src/cappuccino/ide/intellij/plugin/jstypedef/stubs/JsTypeDefStubVersion.kt
@@ -1,7 +1,7 @@
 package cappuccino.ide.intellij.plugin.jstypedef.stubs
 
 object JsTypeDefStubVersion {
-    private const val MINOR_VERSION = 2
+    private const val MINOR_VERSION = 0
     private const val MAJOR_VERSION = 1
     const val VERSION = MINOR_VERSION + MAJOR_VERSION
 }
