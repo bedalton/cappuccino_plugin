c0f4907c4c62eefd260c6eacb54ffd710e26d385
bedalton <dsbadal@gmail.com>
2019-08-20 21:43:31 -0700

    Update JsTypeDefUnknownTypeInspection.kt

--- src/cappuccino/ide/intellij/plugin/jstypedef/inspections/JsTypeDefUnknownTypeInspection.kt
+++ src/cappuccino/ide/intellij/plugin/jstypedef/inspections/JsTypeDefUnknownTypeInspection.kt
@@ -4,7 +4,7 @@ import cappuccino.ide.intellij.plugin.indices.ObjJClassDeclarationsIndex
 import cappuccino.ide.intellij.plugin.indices.ObjJTypeDefIndex
 import cappuccino.ide.intellij.plugin.jstypedef.contributor.JsPrimitives
 import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefClassesByNameIndex
-import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefClassesByNamespaceIndex
+import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefClassesByPartialNamespaceIndex
 import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefKeyListsByNameIndex
 import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefTypeAliasIndex
 import cappuccino.ide.intellij.plugin.jstypedef.lang.JsTypeDefBundle
@@ -13,6 +13,7 @@ import cappuccino.ide.intellij.plugin.jstypedef.psi.interfaces.JsTypeDefClassDec
 import cappuccino.ide.intellij.plugin.jstypedef.psi.interfaces.JsTypeDefHasGenerics
 import cappuccino.ide.intellij.plugin.jstypedef.psi.types.JsTypeDefTypes
 import cappuccino.ide.intellij.plugin.psi.utils.getPreviousNonEmptyNode
+import cappuccino.ide.intellij.plugin.utils.isNotNullOrBlank
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
@@ -35,59 +36,59 @@ class JsTypeDefUnknownTypeInspection : LocalInspectionTool() {
 
     override fun buildVisitor(problemsHolder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : JsTypeDefVisitor() {
-            override fun visitType(type: JsTypeDefType) {
-                super.visitType(type)
-                annotateTypeIfNecessary(type, problemsHolder)
-            }
-
-            override fun visitQualifiedTypeName(o: JsTypeDefQualifiedTypeName) {
-                super.visitQualifiedTypeName(o)
-                annotateIfNecessary(o, problemsHolder)
+            override fun visitTypeName(typeName: JsTypeDefTypeName) {
+                super.visitTypeName(typeName)
+                annotateTypeIfNecessary(typeName, problemsHolder)
             }
         }
     }
 
-    private fun annotateTypeIfNecessary(type:JsTypeDefType, problemsHolder: ProblemsHolder) {
-        if (type.parent is JsTypeDefQualifiedTypeName) {
+    private fun annotateTypeIfNecessary(typeName: JsTypeDefTypeName, problemsHolder: ProblemsHolder) {
+        if (ignoreTypeName(typeName))
             return
-        }
-        val typeName = type.typeName
-        if (
-                type.parent !is JsTypeDefExtendsStatement
-                && type.parent !is JsTypeDefClassDeclaration<*,*>
-                && type.parent !is JsTypeDefHasGenerics
-                && type.parent !is JsTypeDefArrayType
-                && typeName != null
-                && !classExists(type.project, typeName.text)
-                && typeName.text !in type.enclosingGenerics
-        ) {
-            val typesParent = type.getParentOfType(JsTypeDefGenericTypeTypes::class.java)?.parent
-            if (typesParent != null
-                    && (typesParent is JsTypeDefClassDeclaration<*,*> || typesParent is JsTypeDefFunction)
-                    && type.getPreviousNonEmptyNode(true)?.elementType != JsTypeDefTypes.JS_COLON
-            ) {
+        val project = typeName.project
+        val typeNameString = typeName.text
+        val namespaceComponents = typeName.previousSiblings + typeName
+        val namespacedName = if (typeName.parent is JsTypeDefQualifiedTypeName) namespaceComponents.joinToString(".") { it.text } else null
+        if (namespacedName.isNotNullOrBlank()) {
+            if (JsTypeDefClassesByPartialNamespaceIndex.instance.containsKey(namespacedName!!, project)) {
                 return
             }
-            problemsHolder.registerProblem(typeName, JsTypeDefBundle.message("jstypedef.inspections.invalid-type.error.message", typeName.text))
-        }
-    }
-
-    private fun annotateIfNecessary(qualifiedTypeName: JsTypeDefQualifiedTypeName, problemsHolder: ProblemsHolder) {
-        val project = qualifiedTypeName.project
-        val typeNames = qualifiedTypeName.typeNameList
-        if (typeNames.size < 1)
+            problemsHolder.registerProblem(typeName, JsTypeDefBundle.message("jstypedef.inspections.invalid-type.error.message", namespacedName))
             return
-        for (i in 0 .. typeNames.lastIndex) {
-            val qualifiedName = typeNames.subList(0, i).joinToString(".")
-            if (JsTypeDefClassesByNamespaceIndex.instance.getKeysByPattern("$qualifiedName.*", project).isEmpty()) {
-                val typeName = typeNames[i]
-                problemsHolder.registerProblem(typeName, JsTypeDefBundle.message("jstypedef.inspections.invalid-type.error.message", typeName.text))
-            }
         }
+        // Type is existing class
+        if (classExists(project, typeNameString))
+            return
+        // Type References generic type
+        if (typeNameString in (typeName.parent as? JsTypeDefType)?.enclosingGenerics.orEmpty()) {
+            return
+        }
+
+        // Type is itself a generic type
+        if (typeIsGenericTypeDeclaration(typeName)) {
+            return
+        }
+        problemsHolder.registerProblem(typeName, JsTypeDefBundle.message("jstypedef.inspections.invalid-type.error.message", typeNameString))
     }
 
-    private fun classExists(project:Project, typeName:String) : Boolean {
-        return  JsTypeDefClassesByNameIndex.instance.containsKey(typeName, project)
+    private fun typeIsGenericTypeDeclaration(typeName: JsTypeDefTypeName): Boolean {
+        val typesParent = typeName.getParentOfType(JsTypeDefGenericTypeTypes::class.java)?.parent
+        return typesParent != null
+                && (typesParent is JsTypeDefClassDeclaration<*, *> || typesParent is JsTypeDefFunction)
+                && typeName.getPreviousNonEmptyNode(true)?.elementType != JsTypeDefTypes.JS_COLON
+    }
+
+    private fun ignoreTypeName(typeName: JsTypeDefTypeName): Boolean {
+        val type = typeName.parent as? JsTypeDefType ?: return false
+        return type.parent !is JsTypeDefExtendsStatement
+                && type.parent !is JsTypeDefClassDeclaration<*, *>
+                && type.parent !is JsTypeDefHasGenerics
+                && type.parent !is JsTypeDefArrayType
+    }
+
+    private fun classExists(project: Project, typeName: String): Boolean {
+        return JsTypeDefClassesByNameIndex.instance.containsKey(typeName, project)
                 || JsTypeDefTypeAliasIndex.instance.containsKey(typeName, project)
                 || ObjJClassDeclarationsIndex.instance.containsKey(typeName, project)
                 || JsTypeDefKeyListsByNameIndex.instance.containsKey(typeName, project)
@@ -98,7 +99,7 @@ class JsTypeDefUnknownTypeInspection : LocalInspectionTool() {
 
 
     companion object {
-        val LOGGER:Logger by lazy {
+        val LOGGER: Logger by lazy {
             Logger.getLogger("#${JsTypeDefUnknownTypeInspection::class.java.name}")
         }
     }
