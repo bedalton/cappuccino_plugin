9f641e3bcb819ed41560c2496820cdc3a130755f
daniel <dsbadal@gmail.com>
2019-10-19 07:33:00 -0700

    Revert "add inlay hint provider"
    This reverts commit c364b41feb4fbeea0defa69fdc9378f302219dc7.

--- src/cappuccino/ide/intellij/plugin/lang/ObjJFileTypeFactory.kt
+++ src/cappuccino/ide/intellij/plugin/lang/ObjJFileTypeFactory.kt
@@ -6,6 +6,7 @@ import cappuccino.ide.intellij.plugin.jstypedef.lang.JsTypeDefFileType
 import com.intellij.openapi.fileTypes.FileTypeConsumer
 import com.intellij.openapi.fileTypes.FileTypeFactory
 
+@Suppress("DEPRECATION")
 class ObjJFileTypeFactory : FileTypeFactory() {
     override fun createFileTypes(fileTypeConsumer: FileTypeConsumer) {
         fileTypeConsumer.consume(ObjJFileType.INSTANCE, ObjJFileType.FILE_EXTENSION)
--- src/cappuccino/ide/intellij/plugin/psi/interfaces/ObjJUniversalFunctionElement.kt
+++ src/cappuccino/ide/intellij/plugin/psi/interfaces/ObjJUniversalFunctionElement.kt
@@ -1,60 +1,9 @@
 package cappuccino.ide.intellij.plugin.psi.interfaces
 
 import cappuccino.ide.intellij.plugin.hints.ObjJFunctionDescription
-import cappuccino.ide.intellij.plugin.inference.INFERRED_EMPTY_TYPE
-import cappuccino.ide.intellij.plugin.inference.InferenceResult
-import cappuccino.ide.intellij.plugin.inference.JsFunctionType
-import cappuccino.ide.intellij.plugin.inference.toJsTypeList
-import cappuccino.ide.intellij.plugin.jstypedef.contributor.JsTypeDefFunctionArgument
-import cappuccino.ide.intellij.plugin.jstypedef.contributor.JsTypeListType
-import cappuccino.ide.intellij.plugin.psi.ObjJFormalParameterArg
-import cappuccino.ide.intellij.plugin.psi.ObjJFunctionDeclaration
-import cappuccino.ide.intellij.plugin.psi.ObjJFunctionLiteral
-import cappuccino.ide.intellij.plugin.psi.ObjJPreprocessorDefineFunction
-import cappuccino.ide.intellij.plugin.psi.utils.docComment
 import cappuccino.ide.intellij.plugin.universal.psi.ObjJUniversalPsiElement
-import cappuccino.ide.intellij.plugin.utils.isNotNullOrEmpty
-import cappuccino.ide.intellij.plugin.utils.or
 
 interface ObjJUniversalFunctionElement : ObjJUniversalPsiElement {
-    val functionNameString: String?
-    val description: ObjJFunctionDescription
-}
-
-
-fun ObjJFunctionDeclarationElement<*>.toJsTypeListType(): JsTypeListType.JsTypeListFunctionType {
-    val comment = docComment
-    val params = when (this) {
-        is ObjJFunctionDeclaration -> this.formalParameterList?.formalParameterArgList
-        is ObjJFunctionLiteral -> this.formalParameterList?.formalParameterArgList
-        is ObjJPreprocessorDefineFunction -> this.formalParameterList?.formalParameterArgList
-        else -> emptyList()
-    } ?: emptyList()
-
-    val returnTypeString = stub?.returnType
-    val returnTypesList = if (returnTypeString != null)
-        setOf(returnTypeString).toJsTypeList()
-    else
-        comment?.getReturnTypes(project)?.toJsTypeList()
-    val returnType = if (returnTypesList.isNotNullOrEmpty())
-        InferenceResult(types = returnTypesList!!)
-    else INFERRED_EMPTY_TYPE
-
-    return JsTypeListType.JsTypeListFunctionType(
-            name = stub?.functionName ?: functionNameString,
-            comment = null, // @todo implement comment parsing
-            parameters = params.mapIndexed { i, it -> it.toJsNamedProperty(comment?.getParameterComment(i)?.possibleClassStrings) },
-            returnType = returnType
-    )
-}
-
-
-private fun ObjJFormalParameterArg.toJsNamedProperty(typeList: Set<String>?): JsTypeDefFunctionArgument {
-    @Suppress("NAME_SHADOWING") val typeList = typeList?.toJsTypeList() ?: emptySet()
-    return JsTypeDefFunctionArgument(
-            name = this.variableName?.text.or("_"),
-            comment = docComment?.commentText,
-            types = InferenceResult(types = typeList, nullable = true),
-            default = null
-    )
+    val functionNameString:String?
+    val description:ObjJFunctionDescription
 }
