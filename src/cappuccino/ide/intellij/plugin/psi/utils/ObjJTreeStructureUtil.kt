9db129d9573b696edddb5796694b7bb42d725eab
daniel <dsbadal@gmail.com>
2019-10-27 14:03:14 -0700

    fix tree structure view

--- src/cappuccino/ide/intellij/plugin/psi/utils/ObjJTreeStructureUtil.kt
+++ src/cappuccino/ide/intellij/plugin/psi/utils/ObjJTreeStructureUtil.kt
@@ -1,9 +1,11 @@
 package cappuccino.ide.intellij.plugin.psi.utils
 
 import cappuccino.ide.intellij.plugin.psi.*
+import cappuccino.ide.intellij.plugin.psi.interfaces.ObjJFunctionDeclarationElement
 import cappuccino.ide.intellij.plugin.psi.interfaces.ObjJHasTreeStructureElement
 import cappuccino.ide.intellij.plugin.structure.ObjJStructureViewElement
 import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.util.treeView.smartTree.TreeElement
 import com.intellij.navigation.ItemPresentation
 import com.intellij.openapi.progress.ProgressIndicatorProvider
 import icons.ObjJIcons
@@ -13,11 +15,11 @@ object ObjJTreeStructureUtil {
     fun createTreeStructureElement(declaration: ObjJImplementationDeclaration): ObjJStructureViewElement {
         val fileName = ObjJPsiFileUtil.getContainingFileName(declaration)
         val presentation: ItemPresentation = when {
-            declaration.isCategory -> PresentationData("@category ${declaration.getClassName()} (${declaration.categoryNameString})", fileName, ObjJIcons.CATEGORY_ICON, null)
-            declaration.superClassName != null && declaration.superClassName?.isNotEmpty() == true -> PresentationData("@implementation ${declaration.classNameString} : ${declaration.superClassName}", fileName, ObjJIcons.CLASS_ICON, null)
-            else -> PresentationData("@implementation ${declaration.classNameString}", fileName, ObjJIcons.CLASS_ICON, null)
+            declaration.isCategory -> PresentationData("${declaration.getClassName()} (${declaration.categoryNameString})", fileName, ObjJIcons.CATEGORY_ICON, null)
+            declaration.superClassName != null && declaration.superClassName?.isNotEmpty() == true -> PresentationData("${declaration.classNameString} : ${declaration.superClassName}", fileName, ObjJIcons.CLASS_ICON, null)
+            else -> PresentationData("${declaration.classNameString}", fileName, ObjJIcons.CLASS_ICON, null)
         }
-        return ObjJStructureViewElement(declaration, presentation, declaration.classNameString)
+        return ObjJStructureViewElement(declaration, presentation, "__"+declaration.classNameString)
     }
 
     fun getTreeStructureChildElements(declaration: ObjJImplementationDeclaration): Array<ObjJStructureViewElement> {
@@ -34,7 +36,7 @@ object ObjJTreeStructureUtil {
     }
 
     fun createTreeStructureElement(instanceVariable: ObjJInstanceVariableDeclaration): ObjJStructureViewElement {
-        val label = "ivar: ${instanceVariable.formalVariableType.text} ${instanceVariable.variableName?.text
+        val label = "${instanceVariable.formalVariableType.text} ${instanceVariable.variableName?.text
                 ?: "{UNDEF}"}${if (instanceVariable.accessor != null) " @accessors" else ""}"
         val presentation = PresentationData(label, ObjJPsiFileUtil.getContainingFileName(instanceVariable), ObjJIcons.VARIABLE_ICON, null)
         return ObjJStructureViewElement(instanceVariable, presentation, "_" + (instanceVariable.variableName?.text
@@ -43,8 +45,8 @@ object ObjJTreeStructureUtil {
 
     fun createTreeStructureElement(declaration: ObjJProtocolDeclaration): ObjJStructureViewElement {
         val fileName = ObjJPsiFileUtil.getContainingFileName(declaration)
-        val presentation: ItemPresentation = PresentationData("@protocol ${declaration.classNameString}", fileName, ObjJIcons.PROTOCOL_ICON, null)
-        return ObjJStructureViewElement(declaration, presentation, declaration.classNameString)
+        val presentation: ItemPresentation = PresentationData(declaration.classNameString, fileName, ObjJIcons.PROTOCOL_ICON, null)
+        return ObjJStructureViewElement(declaration, presentation, "__"+declaration.classNameString)
     }
 
     fun createTreeStructureElement(header: ObjJProtocolScopedMethodBlock): ObjJStructureViewElement {
@@ -53,14 +55,45 @@ object ObjJTreeStructureUtil {
         return ObjJStructureViewElement(header, PresentationData(text, fileName, null, null), "")
     }
 
-    fun createTreeStructureElement(header: ObjJMethodDeclaration): ObjJStructureViewElement {
-        return createTreeStructureElement(header.methodHeader)
+    fun createTreeStructureElement(declaration: ObjJMethodDeclaration): ObjJStructureViewElement {
+        val fileName = ObjJPsiFileUtil.getContainingFileName(declaration)
+        val presentation: ItemPresentation = PresentationData(declaration.methodHeader.text.replace("[\n\r]*", ""), fileName, ObjJIcons.METHOD_ICON, null)
+        return object:ObjJStructureViewElement(declaration, presentation, declaration.containingClassName) {
+            override fun getChildren(): Array<out TreeElement> {
+                return emptyArray()
+            }
+        }
     }
 
     fun createTreeStructureElement(header: ObjJMethodHeader): ObjJStructureViewElement {
         val fileName = ObjJPsiFileUtil.getContainingFileName(header)
         val presentation: ItemPresentation = PresentationData(header.text.replace("[\n\r]*", ""), fileName, ObjJIcons.METHOD_ICON, null)
         return ObjJStructureViewElement(header, presentation, header.containingClassName)
+    }
+
+    fun createTreeStructureElement(variable:ObjJGlobalVariableDeclaration): ObjJStructureViewElement {
+        val fileName = ObjJPsiFileUtil.getContainingFileName(variable)
+        val presentation: ItemPresentation = PresentationData(variable.variableNameString, fileName, ObjJIcons.GLOBAL_VARIABLE_ICON, null)
+        return ObjJStructureViewElement(variable, presentation, "zzzz_${variable.variableNameString}")
+    }
+
+    fun createTreeStructureElement(variable:ObjJVariableName): ObjJStructureViewElement {
+        if (variable.parent?.parent is ObjJVariableDeclaration) {
+            val dec:ObjJVariableDeclaration? = variable.parent!!.parent as? ObjJVariableDeclaration
+            val functionLiteral = dec?.assignedValue?.leftExpr?.functionLiteral
+            if (functionLiteral != null)
+                return functionLiteral.createTreeStructureElement()
+        }
+        val fileName = ObjJPsiFileUtil.getContainingFileName(variable)
+        val presentation: ItemPresentation = PresentationData(variable.text, fileName, ObjJIcons.VARIABLE_ICON, null)
+        return ObjJStructureViewElement(variable, presentation, "zzzzz_${variable.text}")
+    }
+
+    fun createTreeStructureElement(function:ObjJFunctionDeclarationElement<*>) : ObjJStructureViewElement {
+        val fileName = ObjJPsiFileUtil.getContainingFileName(function)
+        val functionNameString = function.functionNameString.ifEmpty {"function"} +  " (" + function.paramNames.joinToString(", ") +")"
+        val presentation: ItemPresentation = PresentationData(functionNameString, fileName, ObjJIcons.FUNCTION_ICON, null)
+        return ObjJStructureViewElement(function, presentation, "z_${functionNameString}")
     }
 
 }
