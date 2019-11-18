ac82d75095e86e8e2efd5bdff7e8406f6a1d4bd0
daniel <dsbadal@gmail.com>
2019-11-18 13:03:09 -0800

    unify resolve type name references

--- src/cappuccino/ide/intellij/plugin/jstypedef/references/JsTypeDefTypeNameReference.kt
+++ src/cappuccino/ide/intellij/plugin/jstypedef/references/JsTypeDefTypeNameReference.kt
@@ -7,6 +7,7 @@ import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefKeyListsByNameI
 import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefTypeAliasIndex
 import cappuccino.ide.intellij.plugin.jstypedef.indices.JsTypeDefTypeMapByNameIndex
 import cappuccino.ide.intellij.plugin.jstypedef.psi.JsTypeDefTypeName
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
@@ -16,37 +17,41 @@ import com.intellij.psi.ResolveResult
 class JsTypeDefTypeNameReference(element:JsTypeDefTypeName) : PsiPolyVariantReferenceBase<JsTypeDefTypeName>(element, TextRange(0, element.textLength)) {
 
     override fun multiResolve(partial: Boolean): Array<ResolveResult> {
-        val project = element.project
-        val typeName = myElement.text
-        val fromJsTypeDefClass = JsTypeDefClassesByNamespaceIndex.instance[myElement.text, project].mapNotNull {
-            it.typeName
-        }
-
-        val fromTypeMap = JsTypeDefTypeMapByNameIndex.instance[myElement.text, project].mapNotNull {
-            it.typeMapName
-        }
-
-        val fromTypeAlias = JsTypeDefTypeAliasIndex.instance.get(typeName, project).mapNotNull { it.typeName }
-
-        val fromObjJClass = ObjJClassDeclarationsIndex.instance.get(typeName, project).mapNotNull { it.getClassName() }
-
-        val fromKeyList = JsTypeDefKeyListsByNameIndex.instance.get(typeName, project).mapNotNull { it.keyName }
-        val fromObjJTypeDefElement = ObjJTypeDefIndex.instance.get(typeName, project).mapNotNull {
-            it.className
-        }
-        val found:List<PsiElement> = fromJsTypeDefClass +
-                fromTypeMap +
-                fromTypeAlias +
-                fromObjJClass +
-                fromKeyList +
-                fromObjJTypeDefElement
-        return PsiElementResolveResult.createResults(found)
+        return findClassReferencesByName(myElement.text, myElement.project)
     }
+
 
 
     override fun getVariants(): Array<Any> {
         return emptyArray()
     }
 
+    companion object {
+        fun findClassReferencesByName(typeName:String, project:Project): Array<ResolveResult> {
+            val fromJsTypeDefClass = JsTypeDefClassesByNamespaceIndex.instance[typeName, project].mapNotNull {
+                it.typeName
+            }
+
+            val fromTypeMap = JsTypeDefTypeMapByNameIndex.instance[typeName, project].mapNotNull {
+                it.typeMapName
+            }
+
+            val fromTypeAlias = JsTypeDefTypeAliasIndex.instance.get(typeName, project).mapNotNull { it.typeName }
+
+            val fromObjJClass = ObjJClassDeclarationsIndex.instance.get(typeName, project).mapNotNull { it.getClassName() }
+
+            val fromKeyList = JsTypeDefKeyListsByNameIndex.instance.get(typeName, project).mapNotNull { it.keyName }
+            val fromObjJTypeDefElement = ObjJTypeDefIndex.instance.get(typeName, project).mapNotNull {
+                it.className
+            }
+            val found:List<PsiElement> = fromJsTypeDefClass +
+                    fromTypeMap +
+                    fromTypeAlias +
+                    fromObjJClass +
+                    fromKeyList +
+                    fromObjJTypeDefElement
+            return PsiElementResolveResult.createResults(found)
+        }
+    }
 
 }
