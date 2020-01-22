d4229df95e98548e9f922d225110c010a1fcd158
daniel <dsbadal@gmail.com>
2020-01-22 13:38:44 -0800

    Update ObjJFileTreeNode.kt

--- src/cappuccino/ide/intellij/plugin/structure/ObjJFileTreeNode.kt
+++ src/cappuccino/ide/intellij/plugin/structure/ObjJFileTreeNode.kt
@@ -4,8 +4,11 @@ import cappuccino.ide.intellij.plugin.lang.ObjJFile
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
+import com.intellij.openapi.fileTypes.StdFileTypes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.ui.tree.LeafState
 
 class ObjJFileTreeNode(
         myProject: Project,
@@ -18,6 +21,15 @@ class ObjJFileTreeNode(
 
     override fun contains(otherFile: VirtualFile): Boolean {
         return this.virtualFile == otherFile
+    }
+
+    override fun getLeafState(): LeafState {
+        return LeafState.DEFAULT
+    }
+
+    override fun shouldDrillDownOnEmptyElement(): Boolean {
+        val file = this.getValue() as? PsiFile
+        return file != null && file.fileType === StdFileTypes.JAVA
     }
 
     override fun getChildrenImpl(): Collection<AbstractTreeNode<*>> {
