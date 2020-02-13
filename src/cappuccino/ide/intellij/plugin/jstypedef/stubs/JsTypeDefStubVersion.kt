a5f3cbf6b0812204f9b032246bc069e93669eafc
daniel <dsbadal@gmail.com>
2020-02-13 12:52:18 -0800

    update plugin version

--- src/cappuccino/ide/intellij/plugin/jstypedef/stubs/JsTypeDefStubVersion.kt
+++ src/cappuccino/ide/intellij/plugin/jstypedef/stubs/JsTypeDefStubVersion.kt
@@ -2,6 +2,6 @@ package cappuccino.ide.intellij.plugin.jstypedef.stubs
 
 object JsTypeDefStubVersion {
     private const val MINOR_VERSION = 0
-    private const val MAJOR_VERSION = 1
+    private const val MAJOR_VERSION = 2
     const val VERSION = MINOR_VERSION + MAJOR_VERSION
 }
